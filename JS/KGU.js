1fdd95af218ab91f326d0d625ad66158d738139c
KapustinDanil <60873512+KapustinDanil@users.noreply.github.com>
2021-06-27 22:42:35 +0300

    Update KGU.js

--- JS/KGU.js
+++ JS/KGU.js
@@ -4,8 +4,8 @@ console.log(a)
 var tbody=document.getElementById("myTable-2"),tr = "",td = "";
 var ivan = json.KGU0.length;
 for (let i = 0; i < ivan; i++) {   
-  tr=''
-  td='' 
+  tr='';
+  td='';
   td += "<td>" + json["KGU0"][i].KGU + "</td>";
 tr += "<tr>" + td + "</tr>";
 tbody.innerHTML += tr;
