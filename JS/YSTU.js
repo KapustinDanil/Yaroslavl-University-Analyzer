ab87badae1bdc7d211263fd0cf687ee252584500
KapustinDanil <60873512+KapustinDanil@users.noreply.github.com>
2021-06-27 22:42:10 +0300

    Update YSTU.js

--- JS/YSTU.js
+++ JS/YSTU.js
@@ -4,8 +4,8 @@ var a=document.getElementById("banner-root").getAttribute("data-cityId");
 var tbody=document.getElementById("myTable-1"),tr = "",td = "";
 var ivan = json.YSTU0.length;
  for (let i = 0; i < ivan; i++) {   
-     tr=''
-     td='' 
+     tr='';
+     td='';
      td += "<td>" + json["YSTU0"][i].YSTU + "</td>";
  tr += "<tr>" + td + "</tr>";
  tbody.innerHTML += tr;
