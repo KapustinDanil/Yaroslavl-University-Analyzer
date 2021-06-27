bd23e4002f18fa8e501d27de1c45ba90f090071d
KapustinDanil <60873512+KapustinDanil@users.noreply.github.com>
2021-06-27 22:43:03 +0300

    Update Agregator.js

--- JS/Agregator.js
+++ JS/Agregator.js
@@ -4,8 +4,8 @@ var a=document.getElementById("banner-root").getAttribute("data-cityId");
 var tbody=document.getElementById("myTable-5"),tr = "",td = "";
 var ivan = json.Agregator0.length;
  for (let i = 0; i < ivan; i++) {   
-     tr=''
-     td='' 
+     tr='';
+     td='';
      td += "<td>" + json["Agregator0"][i].agregator + "</td>";
  tr += "<tr>" + td + "</tr>";
  tbody.innerHTML += tr;
