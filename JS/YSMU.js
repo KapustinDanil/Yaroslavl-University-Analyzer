5e96ac7889aeff90508bf04243dee9e95c63aa10
KapustinDanil <60873512+KapustinDanil@users.noreply.github.com>
2021-06-27 22:42:21 +0300

    Update YSMU.js

--- JS/YSMU.js
+++ JS/YSMU.js
@@ -4,8 +4,8 @@ console.log(a)
 var tbody=document.getElementById("myTable-4"),tr = "",td = "";
 var ivan = json.YSMU0.length;
 for (let i = 0; i < ivan; i++) {   
-  tr=''
-  td='' 
+  tr='';
+  td='';
   td += "<td>" + json["YSMU0"][i].YSMU + "</td>";
 tr += "<tr>" + td + "</tr>";
 tbody.innerHTML += tr;
